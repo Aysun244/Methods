bb44105180698de987902d798f5d9c0f3be65730
Aysun Naghizada <aysunin-bb104@code.edu.az>
2025-03-13 22:08:18 +0400

    tersmetn

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Metn:"); 
+        string metn = Console.ReadLine();
+
+        string tersmetn = new
+            string(metn.Reverse().ToArray());
+        Console.WriteLine("Metnin tersi:"+tersmetn);
+    }
+}
